46a351eb9de103eec9f21a562a58f819dfca999e
magongit <47392001+magongit@users.noreply.github.com>
2020-07-20 15:44:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Client } = require('pg');
-const connectionString = 'postgres://user:password@postgresql:5432/sampledb';
+const connectionString = 'postgres://user:password@database:5432/sampledb';
 
 const client = new Client({
     connectionString: connectionString
